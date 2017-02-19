4e07409f34cf8221d91db7ec845eb211e411e837
Julien-laville <alternative451@myopera.com>
2017-02-19 15:09:30 +0100

    moard data

--- cv.js
+++ cv.js
@@ -1,126 +1,133 @@
 function getData() {
     return {
-        technologies : {
-            languages : [
-                "JAVA", "JavaScript + NodeJS", "XML", "JSON", "C", "C++", "HTML5", "CSS3", "Ruby", "PHP"
-            ],
-            "Framework &amp; CMS" : [
-                "Ruby on rails", "Wordpress", "Spring", "Hibernate", "jQuery", "Prototype", "ExtJs", "ExpressJS"
-            ],
-            "Systèmes" : [
-                "Windows", "Linux"
-            ],
-            "Bases de Données" : [
-                "MySQL", "SQLite"
-            ],
-            "Méthodes" : [
-                "AJAX", "AJAJ", "WebService (SOAP + WSDL)"
-            ],
-            "Standards et Normes" : [
-                "Pattern GOF", "MVC",  "J2EE", "Java RMI", "Proactive", "Cross-domain AJAX via JSONP", "CORS"
-            ], 
-            "Outils" : [
-                "Eclipse", "NetBeans", "Redmine", "JUnit", "Rubymine", "Tomcat", "Apache HTTP serveur", "SVN", "Mercurial", "GIT", "Grunt", "Capistrano", "Travis CI", "Active-record"
-            ]
-        },
-        "experiences" : [{
-            "Société" : "Qwant",
-            "dates" : {from : "October 1, 2014", to : Date.now()},
-            "poste" : "Ingénieur Recherche et développement",
-            "Réalisation" : ["Refonte du front-end du site web <a href=\"http://qwant.com\">qwant.com</a>, refonte stack Internationalisation, automatisation des builds et automatisation du contrôle qualité en collaboration avec la CI","Mise en place de Qwant Lite, la version allégé de qwant pour robots, et anciens navigateurs en ExpressJS."],
-            "contexte" : ["One page app", "vanilla JS", "lightweight", "architecture 3 tiers", "CI"],
-            "Environement technique" : ["JavaScript", "NodeJs", "ExpressJS", "Grunt", "GIT", "PHPStorm", "Vagrant", "Docker", "Poedit"]            
-        }, {
-            "Société" : "CrossKnowledge Epistema",
-            "dates" : {from : "2014-04-00", to : "2014-09-00"},
-            "poste" : "Ingénieur Recherche et développement",
-            "Réalisation" : ["Dans le cadre d’une refonte de l’application web, Intégration complete du framework ExtJs pour les éléments de navigation, graphiques, calendriers"],
-            "Environement technique" : ["JavaScript", "PHP", "Mercurial", "ExtJS"]  
-            
+        technologies: [{
+            category : "languages",
+            list : ["JAVA", "JavaScript + NodeJS", "XML", "JSON", "C", "C++", "HTML5", "CSS3", "Ruby", "PHP"]
         },{
-            "Société" : "Air France, Sophia Antipolis",
-            "dates" : {from : "2014-04-00", to : "2014-09-00"},
-            "poste" : "Ingénieur Recherche et développement",
-            "Réalisation" : ["Dans le cadre d’une refonte de l’application web, Intégration complete du framework ExtJs pour les éléments de navigation, graphiques, calendriers"],
-            "Environement technique" : ["JavaScript", "PHP", "Mercurial", "ExtJS"]  
-        }]
+            category : "Framework &amp; CMS",
+            list : ["Ruby on rails", "Wordpress", "Spring", "Hibernate", "jQuery", "Prototype", "ExtJs", "ExpressJS"]
+        },{
+            category : "Systèmes",
+            list : ["Windows", "Linux"]
+        }, {
+            category: "Bases de Données",
+            list: ["MySQL", "SQLite"]
+        }, {
+            category : "Méthodes",
+            list : ["AJAX", "AJAJ", "WebService (SOAP + WSDL)"]
+        },{
+            category : "Standards et Normes",
+            list : ["Pattern GOF", "MVC", "J2EE", "Java RMI", "Proactive", "Cross-domain AJAX via JSONP", "CORS"]
+        }, {
+            category : "Outils",
+            list : ["Eclipse", "NetBeans", "Redmine", "JUnit", "Rubymine", "Tomcat", "Apache HTTP serveur", "SVN", "Mercurial", "GIT", "Grunt", "Capistrano", "Travis CI", "Active-record"]
+        }],
+        "experiences": [{
+            "Société": "Qwant",
+            "dates": {from: "October 1, 2014", to: Date.now()},
+            "poste": "Ingénieur Recherche et développement",
+            "Réalisation": ["Refonte du front-end du site web <a href=\"http://qwant.com\">qwant.com</a>, refonte stack Internationalisation, automatisation des builds et automatisation du contrôle qualité en collaboration avec la CI", "Mise en place de Qwant Lite, la version allégé de qwant pour robots, et anciens navigateurs en ExpressJS."],
+            "contexte": ["One page app", "vanilla JS", "lightweight", "architecture 3 tiers", "CI"],
+            "Environement technique": ["JavaScript", "NodeJs", "ExpressJS", "Grunt", "GIT", "PHPStorm", "Vagrant", "Docker", "Poedit"]
+        }, {
+            "Société": "CrossKnowledge Epistema",
+            "dates": {from: "2014-04-00", to: "2014-09-00"},
+            "poste": "Ingénieur Recherche et développement",
+            "Réalisation": ["Dans le cadre d’une refonte de l’application web, Intégration complete du framework ExtJs pour les éléments de navigation, graphiques, calendriers"],
+            "Environement technique": ["JavaScript", "PHP", "Mercurial", "ExtJS"]
+
+        }, {
+            "Société": "Air France, Sophia Antipolis",
+            "dates": {from: "2013-07-00", to: "2014-03-00"},
+            "poste": "Développeur informatique",
+            "Réalisation": ["étude, développement et tests autour des sites Business to Client de la société"],
+            "Environement technique": ["JavaScript", "jQuery", "struts", "J2EE", "Perl"]
+        }, {
+            "Société": "Koedia - Sophia Antipolis - (06)",
+            "dates": {from: "2012-02-00", to: "2013-06-00"},
+            "poste": "Développeur informatique",
+            "Réalisation": ["Backend : Ajout de fonctionnalités aux WebService d’aviation : Rédaction des spécifications, développement des tests et des services.", "Frontend : Monitoring, Ajout de fonctionnalités et optimisation des sites B2B &amp; B2C.<br> Extension des fonctionnalités ticket-tracker Redmine"],
+            "Environement technique": ["WebService (SOAP &amp; WSDL)", "Java J2EE", "Hibernate", "Tomcat", "Spring", "HTML", "Json", "AJAX", "Ruby &amp; Ruby on Rails"]
+        }, {
+            "Société": "Auto-entreprise de développement WEB",
+            "dates": {from: "2009-01-00", to: "2012-01-00"},
+            "poste": "Développeur informatique",
+            "Réalisation": ["Réalisation d’un site portail et boutique en ligne"],
+            "Environement technique": ["Ruby on rails", "Eclipse", "JavaScript", "jQuery UI", "jQuery", "Prototype", "HTML", "Json", "AJAX", "API Dropbox"]
+        }, {
+            "Société": "I3S, Sophia-Antipolis - (06)",
+            "dates": {from: "2011-06-00", to: "2011-08-00"},
+            "poste": "Développeur (Stagiaire)",
+            "Réalisation": ["Réalisations d’une application de résolution de la dynamique de réseaux génétiques basée sur la logique de Hoare."],
+            "Environement technique": ["Windows", "Eclipse", "Java"]
+
+        }, {
+            "Société": "Université de Nice-Sophia Antipolis, Labarotoire des Langues Appliqués - (06)",
+            "dates": {from: "2010-06-00", to: "2010-08-00"},
+            "poste": "Développeur (Stagiaire)",
+            "Réalisation": ["Réalisations d’une application de résolution de la dynamique de réseaux génétiques basée sur la logique de Hoare.<br>Conception et Création d’un serious game s’articulant en trois parties: jeu, éditeur, serveur de<br>scénario"],
+            "Environement technique": ["CMS WordPress", "PHP", "HTML5", "Python", "XML", "JavaScript", "AJAX"]
+        }, {
+            "Société": "Arkadia Consulting, Nice - (06)",
+            "dates": {from: "2009-06-00", to: "2009-08-00"},
+            "poste": "Développeur (Stagiaire)",
+            "Réalisation": ["Réalisation de deux sites « vitrines » en Ruby on Rails, ces sites on été construits sous formes de CMS, afin de permettre leur maintenance sans développeur."],
+            "Environement technique": ["Windows", "Netbeans", "Ruby on Rails", "jQuery", "Photoshop"]
+
+        }],
+        "Diplômes": [
+            {
+                date: 2011,
+                "title": "Master 2 Informatique, fondements et ingénierie, - Université de Nice-Sophia Antipolis"
+            },
+            {date: 2009, "title": "Licence Mathématiques &amp; Informatique, Université de Nice-Sophia Antipolis"},
+            {date: 2005, "title": "Baccalauréat Scientifique, Lycée Carnot, Cannes"}
+        ],
+
+        "Ouvrages &amp; lectures": [
+            {title: "Tout sur le code", author: "Steve McConnell", ISBN: "2100487531"},
+            {title: "JUnit in Action", author: "Peter Tahchiev", ISBN: "1935182021"},
+            {title: "Learning JavaScript Design Patterns", author: "", ISBN: "1449334864"},
+            {title: "97 Things Every Programmer Should Know", author: "", ISBN: "0596809485"}
+        ],
+        "Hobbies": [
+            "Multirotor R/C",
+            "Impression 3D (repRap)",
+            "Électronique (microcontrôleur)"
+        ],
+
+        "µ projets": [
+            {
+                title: "KV-Mini host",
+                desc: "server simple de stockage KEY VALUE.",
+                href: "https://github.com/Julien-laville/kv-mini-host",
+                url: "Github/kv-mini-host"
+            },
+            {
+                title: "Divide",
+                desc: "Mini jeu JavaScript inspiré de Super Hot (W.I.P).",
+                href: "https://github.com/Julien-laville/divide/tree/gh-pages",
+                url: "Github/divide"
+            },
+            {
+                title: "jsToolBox",
+                desc: "Micro lib JavaScript orienté Haute performance.",
+                href: "https://github.com/Julien-laville/jsToolBox",
+                url: "Github/jsToolBox"
+            }
+        ]
     }
 }
-/*
-<h3><a id="Air_France_Sophia_Antipolis__06_46"></a>)</h3>
-<p><em>Juillet 2013 - Mars 2014.</em></p>
-<p><strong>Poste occupé</strong> : Ingénieur d’étude.</p>
-<p><strong>Réalisations</strong> : étude, développement et tests autour des sites Business to Client de la société.</p>
-<p><strong>Environnement technique</strong> : javascript, jQuery, struts, J2EE, Perl.</p>
-<h3><a id="Koedia__Sophia_Antipolis__06_55"></a>Koedia - Sophia Antipolis - (06)</h3>
-<p><em>Février 2012 - juin 2013.</em></p>
-<p><strong>Poste occupé</strong> : Développeur informatique.</p>
-<p><strong>Projets</strong> :<br>
-<ul><li>Backend : Ajout de fonctionnalités aux WebService d’aviation : Rédaction des spécifications, développement des tests et des services.</li>
 
-    <li>Frontend : Monitoring, Ajout de fonctionnalités et optimisation des sites B2B &amp; B2C.<br>
-        Extension des fonctionnalités ticket-tracker Redmine.</li></ul>
-<p><strong>Environnement technique</strong> : WebService (SOAP &amp; WSDL), Java J2EE, Hibernate, Tomcat, Java J2EE, Spring, HTML, Json, AJAX, Tomcat, Ruby &amp; Ruby on Rails</p>
-<h3><a id="Autoentreprise_de_dveloppement_WEB_68"></a>Auto-entreprise de développement WEB</h3>
-<p><em>2009 - 2012</em></p>
-<p><strong>Projet</strong> : Réalisation d’un site portail et boutique en ligne.</p>
-<p><strong>Environnement technique</strong> : Ruby on rails, Eclipse, JavaScript, jQuery, AJAX, jQuery UI, Prototype, The Gimp, API Dropbox.</p>
-<h3><a id="I3S_SophiaAntipolis__06_75"></a>I3S, Sophia-Antipolis - (06)</h3>
-<p><em>juin 2011 – aout 2011</em></p>
-<p><strong>Poste occupé</strong> : Développeur (Stagiaire).</p>
-<p><strong>Projet</strong> : Réalisations d’une application de résolution de la dynamique de réseaux génétiques basée sur la logique de Hoare.</p>
-<p><strong>Environnement technique</strong> : Windows, Eclipse, Java.</p>
-<h3><a id="Universit_de_NiceSophia_Antipolis_Labarotoire_des_Langues_Appliqus__06_84"></a>Université de Nice-Sophia Antipolis, Labarotoire des Langues Appliqués - (06)</h3>
-<p><em>juin 2010 – août 2010</em></p>
-<p><strong>Poste occupé</strong> : Développeur (Stagiaire).</p>
-<p><strong>Projet</strong> : Conception et réalisation d’un outil collaboratif entre les élèves et les enseignants de LEA. Gestion d’un portefeuille de traduction, traduction et correction collaborative.<br>
-Conception et Création d’un serious game s’articulant en trois parties: jeu, éditeur, serveur de<br>
-scénario.</p>
-<p><strong>Environnement technique</strong> : CMS WordPress, PHP, HTML5, Apache HTTP Server, Python, XML, PHP, HTML5, JavaScript, AJAX.</p>
-<h3><a id="Arkadia_Consulting_Nice__06_95"></a>Arkadia Consulting, Nice - (06)</h3>
-<p><em>juin 2009 – août  2009</em></p>
-<p><strong>Poste occupé</strong> : Développeur (Stagiaire).</p>
-<p><strong>Projet</strong> : Réalisation de deux sites « vitrines » en Ruby on Rails, ces sites on été construits sous formes de CMS, afin de permettre leur maintenance sans développeur.</p>
-<p><strong>Environnement technique</strong> : Windows, Netbeans, Ruby on Rails, jQuery, Photoshop.</p>
-<h2><a id="Langues_104"></a>Langues</h2>
-<p><strong>Anglais</strong> : TOEIC 790.</p>
-<h2><a id="Diplmes_107"></a>Diplômes</h2>
-<p><em>2011</em> - Master 2 Informatique, fondements et ingénierie, - Université de Nice-Sophia Antipolis</p>
-<p><em>2009</em> - Licence Mathématiques &amp; Informatique, Université de Nice-Sophia Antipolis</p>
-<p><em>2005</em> - Baccalauréat Scientifique, Lycée Carnot, Cannes</p>
-<h2><a id="Ouvrages__lectures_114"></a>Ouvrages &amp; lectures</h2>
-<p><strong>Coder proprement</strong> De Robert C. Martin (ISBN 2744025836)</p>
-<p><strong>Tout sur le code</strong> De Steve McConnell (ISSN 2100487531)</p>
-<p><strong>JUnit in Action</strong> De Peter Tahchiev (ISBN 1935182021)</p>
-<p><strong>Learning JavaScript Design Patterns</strong> (EBOOK ISBN 978 1449334864)</p>
-<p><strong>97 Things Every Programmer Should Know</strong> (EBOOK ISBN 978 0596809485)</p>
-<h2><a id="Hobbies_125"></a>Hobbies</h2>
-<p>Multirotor R/C<br>
-Impression 3D (repRap)<br>
-Électronique (microcontrôleur)</p>
-        
-        <h2><a id="Hobbies_125"></a>µ projet</h2>
-        <ul class="project">
-            <li>
-                <h3>KV-Mini host</h3>
-                <p>
-                    server simple de stockage KEY VALUE.
-                </p>
-                <a href="https://github.com/Julien-laville/kv-mini-host">Github/kv-mini-host</a>
-            </li>
-            <li>
-                <h3>Divide</h3>
-                <p>
-                    Mini jeu JavaScript inspiré de Super Hot (W.I.P).
-                </p>
-                <a href="https://github.com/Julien-laville/divide/tree/gh-pages">Github/divide</a>
-            </li>        
-            <li>
-                <h3>jsToolBox</h3>
-                <p>
-                    Micro lib JavaScript orienté Haute performance.
-                </p>
-                <a href="https://github.com/Julien-laville/jsToolBox">Github/jsToolBox</a>
-            </li>               
-        </ul>
- */
+d3.select("body")
+    .selectAll("div")
+    .data(getData().technologies)
+    .enter()
+    .append('strong')
+    .text(function(technology) {
+        return technology.category
+    })
+    .append('span')
+    .text(function(technology) {
+        return technology.list.join(', ')
+    })
